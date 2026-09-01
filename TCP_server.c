ac9a302297eafcd82e9e87871b2bc3b91a089d3c
LuukXu <12503877@student.pxl.be>
2026-09-01 19:28:10 +0200

    TCPServerUpdate

--- TCP_server.c
+++ TCP_server.c
@@ -1,142 +1,109 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <windows.h>
 #include <stdint.h>
-#include <process.h>
- 
+#include <winsock2.h>
+#include <windows.h> // Toegevoegd voor threads
+#include <time.h>
+
 #pragma comment(lib, "ws2_32.lib")
- 
-#define PORT    5006
-#define BACKLOG 10
- 
-typedef struct {
-    SOCKET             sock;
-    struct sockaddr_in addr;
-} ClientInfo;
- 
-unsigned __stdcall client_thread(void *arg) {
-    ClientInfo *info   = (ClientInfo *)arg;
-    SOCKET client_sock = info->sock;
-    char  *client_ip   = inet_ntoa(info->addr.sin_addr);
-    int    client_port = ntohs(info->addr.sin_port);
-    free(info);
-    srand((unsigned)time(NULL) ^ (unsigned)GetCurrentThreadId());
-    int secret = rand() % 1000000 + 1;
-    printf("\n[+] Client verbonden: %s:%d  |  Geheim getal: %d\n",
-           client_ip, client_port, secret);
- 
-    int spel_actief = 1;
-    while (spel_actief) {
-        uint32_t net_val         = 0;
-        int      bytes_ontvangen = 0;
-        char    *buf             = (char *)&net_val;
- 
-        while (bytes_ontvangen < 4) {
-            int n = recv(client_sock, buf + bytes_ontvangen,
-                         4 - bytes_ontvangen, 0);
-            if (n <= 0) {
-                printf("    [%s:%d] Verbinding verbroken.\n",
-                       client_ip, client_port);
-                spel_actief = 0;
-                break;
-            }
-            bytes_ontvangen += n;
+
+#define PORT 12345
+
+// --- DEZE FUNCTIE DRAAIT VOOR ELKE SPELER APART OP DE ACHTERGROND ---
+DWORD WINAPI HandleSpeler(LPVOID lpParam) {
+    // 1. Haal de specifieke client_socket op en maak het geheugen vrij
+    SOCKET client_socket = *(SOCKET*)lpParam;
+    free(lpParam);
+
+    // 2. Iedere thread genereert een EIGEN geheim getal
+    // We gebruiken de Thread ID om ervoor te zorgen dat ze echt andere getallen krijgen
+    srand((unsigned int)(time(NULL) + GetCurrentThreadId()));
+    int target_number = (rand() % 100) + 1;
+    
+    DWORD thread_id = GetCurrentThreadId();
+    printf("[Thread %lu] Speler begonnen! Geheim getal is: %d\n", thread_id, target_number);
+
+    // 3. De spel-lus voor DEZE specifieke speler
+    while (1) {
+        uint32_t net_guess;
+        
+        int bytes_received = recv(client_socket, (char*)&net_guess, sizeof(net_guess), 0);
+        
+        if (bytes_received <= 0) {
+            printf("[Thread %lu] Speler heeft de verbinding verbroken.\n", thread_id);
+            break; 
         }
- 
-        if (!spel_actief) break;
- 
-        int guess = (int)ntohl(net_val);
-        printf("    [%s:%d] Gok: %d\n", client_ip, client_port, guess);
- 
-        const char *antwoord;
-        if (guess < secret)
-            antwoord = "Hoger\n";
-        else if (guess > secret)
-            antwoord = "Lager\n";
-        else
-            antwoord = "Correct\n";
- 
-        send(client_sock, antwoord, (int)strlen(antwoord), 0);
- 
-        if (guess == secret) {
-            printf("    [%s:%d] Correct geraden!\n", client_ip, client_port);
-            spel_actief = 0;
+
+        int guess = ntohl(net_guess);
+        printf("[Thread %lu] Speler gokt: %d\n", thread_id, guess);
+
+        if (guess < target_number) {
+            const char* msg = "Hoger";
+            send(client_socket, msg, strlen(msg), 0);
+        } 
+        else if (guess > target_number) {
+            const char* msg = "Lager";
+            send(client_socket, msg, strlen(msg), 0);
+        } 
+        else {
+            const char* msg = "Correct";
+            send(client_socket, msg, strlen(msg), 0);
+            printf("[Thread %lu] Speler heeft gewonnen! Verbinding gesloten.\n", thread_id);
+            break; 
         }
     }
- 
-    closesocket(client_sock);
-    printf("[-] Client losgekoppeld: %s:%d\n", client_ip, client_port);
+
+    closesocket(client_socket);
     return 0;
 }
- 
-int main(void) {
+
+int main() {
     WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        fprintf(stderr, "WSAStartup mislukt: %d\n", WSAGetLastError());
-        return 1;
-    }
- 
-    srand((unsigned)time(NULL));
- 
-    SOCKET server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (server_sock == INVALID_SOCKET) {
-        fprintf(stderr, "socket() fout: %d\n", WSAGetLastError());
-        WSACleanup(); return 1;
-    }
- 
-    int opt = 1;
-    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(opt));
- 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family      = AF_INET;
+    SOCKET server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    int client_len = sizeof(client_addr);
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) return 1;
+
+    server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port        = htons(PORT);
- 
-    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        fprintf(stderr, "bind() fout: %d\n", WSAGetLastError());
-        closesocket(server_sock); WSACleanup(); return 1;
-    }
- 
-    if (listen(server_sock, BACKLOG) == SOCKET_ERROR) {
-        fprintf(stderr, "listen() fout: %d\n", WSAGetLastError());
-        closesocket(server_sock); WSACleanup(); return 1;
-    }
- 
-    printf("TCP-server 'Hoger/Lager' gestart op poort %d\n", PORT);
-    printf("Wacht op verbindingen (meerdere clients tegelijk)...\n");
- 
-    for (;;) {
-        struct sockaddr_in client_addr;
-        int addr_len = sizeof(client_addr);
- 
-        SOCKET client_sock = accept(server_sock,
-                                    (struct sockaddr *)&client_addr,
-                                    &addr_len);
-        if (client_sock == INVALID_SOCKET) {
-            fprintf(stderr, "accept() fout: %d\n", WSAGetLastError());
-            continue;
-        }
- 
-        ClientInfo *info = malloc(sizeof(ClientInfo));
-        info->sock = client_sock;
-        info->addr = client_addr;
- 
-        HANDLE hThread = (HANDLE)_beginthreadex(NULL, 0, client_thread, info, 0, NULL);
-        if (hThread == NULL) {
-            fprintf(stderr, "Thread aanmaken mislukt\n");
-            closesocket(client_sock);
-            free(info);
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) return 1;
+
+    // SOMAXCONN laat Windows bepalen hoeveel mensen er maximaal in de wachtrij mogen staan
+    listen(server_socket, SOMAXCONN); 
+    printf("Multithreaded TCP-server 'Hoger/Lager' gestart op poort %d...\n", PORT);
+
+    // De Hoofd-lus accepteert ALLEEN inkomende verbindingen
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_len);
+        if (client_socket == INVALID_SOCKET) continue;
+
+        printf("\nNieuwe inkomende verbinding geaccepteerd (IP: %s)\n", inet_ntoa(client_addr.sin_addr));
+
+        // We alloceren een klein stukje geheugen voor de socket. 
+        // Dit is heel belangrijk bij multithreading zodat clients niet elkaars socket overschrijven!
+        SOCKET* new_sock = (SOCKET*)malloc(sizeof(SOCKET));
+        *new_sock = client_socket;
+
+        // Start de thread voor deze specifieke client
+        HANDLE thread = CreateThread(NULL, 0, HandleSpeler, (LPVOID)new_sock, 0, NULL);
+        
+        if (thread) {
+            // Sluit de handle, de thread draait op de achtergrond gewoon door (detach)
+            CloseHandle(thread); 
         } else {
-            CloseHandle(hThread);
+            printf("Fout bij aanmaken thread.\n");
+            free(new_sock);
+            closesocket(client_socket);
         }
     }
- 
-    closesocket(server_sock);
+
+    closesocket(server_socket);
     WSACleanup();
     return 0;
 }
